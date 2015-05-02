a58a5145a119e85a9706c5b3c278b3df864b252b
hii <hii@Lenovo-PC>
2015-05-02 21:39:26 +0200

    Prueba

---
+++ ServiciosSociales-war/src/java/app/managedbeans/CitaJSFManagedBean.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.managedbeans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author hii
+ */
+@ManagedBean
+@RequestScoped
+public class CitaJSFManagedBean {
+
+    /**
+     * Creates a new instance of CitaJSFManagedBean
+     */
+    public CitaJSFManagedBean() {
+    }
+    
+}
